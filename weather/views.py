0bc081698b9a504841a0e1ee73cc0f5393a909db
Diya Jaiswal <shubhijaiswal2000@gmail.com>
2020-06-07 21:01:35 +0530

    Update views.py

--- weather/views.py
+++ weather/views.py
@@ -1,53 +1,68 @@
 import requests
-from django.shortcuts import render
-from .models import City 
+from django.shortcuts import render, redirect
+from .models import City
 from .forms import CityForm
 
-def index(request):  
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'   
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
 
-    err_msg='' 
+    err_msg = ''
+    message = ''
+    message_class = ''
 
-    if request.method =='POST' :
-        form = CityForm(request.POST)  
+    if request.method == 'POST':
+        form = CityForm(request.POST)
 
         if form.is_valid():
-            new_city = form.cleaned_data['name'] 
-            existing_city_count = City.objects.filter(name=new_city).count() 
-            if existing_city_count==0:
-                
-                print(r)
-                form.save() 
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+            
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist in the world!'
             else:
-                err_msg= 'City already exists in the database!'
+                err_msg = 'City already exists in the database!'
 
-    form=CityForm() 
-    cities=City.objects.all()  
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added successfully!'
+            message_class = 'is-success'
 
-    weather_data=[]
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
 
     for city in cities:
-        r = requests.get(url.format(city)).json() 
-        city_weather= { 
-            'city': city.name ,    #name bcoz we want name not object (models.py)
-            'temperature': ((r['main']['temp'] - 32)*5)/9,
-            'description': r['weather'][0]['description'], 
-            'icon': r['weather'][0]['icon'],
-             }  
-        weather_data.append(city_weather) 
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    context = {
+        'weather_data' : weather_data, 
+        'form' : form,
+        'message' : message,
+        'message_class' : message_class
+    }
+
+    return render(request, 'weather/weather.html', context)
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
     
-    print(weather_data)
-
-    #print(city_weather) 
-
-    context = {'weather_data': weather_data , 'form' : form }
-    return render(request,'weather/weather.html', context ) 
-
-
-    #{"coord":{"lon":-115.15,"lat":36.17},
-    #"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],
-    #"base":"stations",
-    #"main":{"temp":46.26,"feels_like":40.46,"temp_min":42.8,"temp_max":48,"pressure":1018,"humidity":52},
-    #"visibility":16093,"wind":{"speed":3.36,"deg":260},"clouds":{"all":1},"dt":1577906007,
-    #"sys":{"type":1,"id":3527,"country":"US","sunrise":1577890294,"sunset":1577925370},
-    #"timezone":-28800,"id":5506956,"name":"Las Vegas","cod":200}
+    return redirect('home')
