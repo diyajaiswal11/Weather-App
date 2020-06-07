b3096b88a07fcf8b3b54c7a2e7457f50ef9a3ac3
Diya Jaiswal <shubhijaiswal2000@gmail.com>
2020-06-07 21:03:07 +0530

    Update urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-     
-    path('',views.index),
+    path('',views.index,name='home'),
+    path('delete/<city_name>/',views.delete_city, name='delete_city')
 ]
